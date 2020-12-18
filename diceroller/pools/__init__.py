7740fee6cac73a4355393f6ea777abb4ac94fed2
rmellema <rene.mellema@gmail.com>
2020-12-18 22:51:55 +0100

    Added a pool for constant numbers

---
+++ diceroller/pools/__init__.py
@@ -0,0 +1,28 @@
+"""
+A module to represent dice pools before they are rolled.
+"""
+from .base import AbstractDicePool
+from .dice import DiceTypePool, DiceDropPool
+from .operator import *
+
+class NumberPool(AbstractDicePool):
+    """
+    A class to represent single numbers without rolling.
+    """
+    def __init__(self, n):
+        self.number = n
+
+    def __str__(self):
+        return str(self.number)
+
+    def roll(self):
+        return self.number, []
+
+    @property
+    def values(self):
+        return set([self.number])
+
+    def probability(self, value):
+        if value != self.number:
+            return 0
+        return 1
