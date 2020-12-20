af47fafe71d4690bcb92e2731abcdf768e2b3295
rmellema <rene.mellema@gmail.com>
2020-12-20 12:58:05 +0100

    Started to define a few utility functions

--- diceroller/__init__.py
+++ diceroller/__init__.py
@@ -1,2 +1,24 @@
-from . import pools
+"""
+This module provides ways to define and combine pools of dice so they can be analyzed.
+"""
+import pathlib
+from lark import Lark
+
+from . import pools, transformer
 __VERSION__ = '0.0.1'
+
+_grammar_file = pathlib.Path(__file__).parent.absolute() / 'grammar.lark'
+_parser = Lark(_grammar_file)
+_transformer = transformer.DicePoolTransformer()
+
+def read_dice(string):
+    "Read a dice pool from a string using standard dice notation."
+    return _transformer.transform(_parser.parse(string))
+
+def roll_dice(string):
+    "Read in a dice pool from a string and roll it."
+    return read_dice(string).roll()
+
+def dice(sides, amount=1):
+    "Create a simple dice pool"
+    return pools.DiceTypePool(sides, amount)
